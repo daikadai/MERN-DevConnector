39963b882adf1578ccbc17d276fef46ae7b88d76
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-06-29 14:18:20 +0700

    6. Implementing JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -45,10 +47,25 @@ router.post('/', [
 
     user.password = await bcrypt.hash(password, salt);
 
+    // Save user in DB
     await user.save();
 
     // Return jsonwebtoken
-    res.send('User registered');
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 36000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token })
+      }
+    )
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Server error');
