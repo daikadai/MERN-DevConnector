96a712fc5727ec72fe92106e1bcaa253390d48c1
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-07-01 14:01:09 +0700

    24. React Router Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Fragment } from 'react'
 import './App.css'
 import Navbar from './components/layout/Navbar'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing'
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Fragment>
+    </Router>
   )
 }
 
