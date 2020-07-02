841d4eadecda704951d1c7eda3736e6b8fc190eb
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-07-02 10:19:04 +0700

    30. Load User & Set Auth Token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import './App.css'
 import Navbar from './components/layout/Navbar'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -8,8 +8,19 @@ import Register from './components/auth/Register';
 import { Provider } from 'react-redux';
 import store from './store';
 import Alert from './components/layout/Alert';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, [])
+
   return (
     <Provider store={store}>
       <Router>
