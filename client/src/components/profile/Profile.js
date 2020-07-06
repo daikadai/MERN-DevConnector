d3c4af34f967bebe2a9a5beaeda7935834e56829
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-07-06 16:59:42 +0700

    48. Displaying Github Repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 
 const Profile = ({ getProfileById, match, auth, profile: { profile, loading } }) => {
   useEffect(() => {
@@ -51,6 +52,10 @@ const Profile = ({ getProfileById, match, auth, profile: { profile, loading } })
             ) :
               (<h4>No education credential</h4>)}
           </div>
+
+          {profile.githubusername && (
+            <ProfileGithub username={profile.githubusername} />
+          )}
         </div>
       </Fragment>}
     </Fragment>
