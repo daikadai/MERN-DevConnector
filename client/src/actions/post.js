58cd7718fb121e4cc34960b04eca3b88a5de1b66
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-07-07 15:01:31 +0700

    56. Comment Display & Delete

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -140,7 +140,7 @@ export const addComment = (postId, formData) => async dispatch => {
 // Delete comment
 export const deleteComment = (postId, commentId) => async dispatch => {
   try {
-    const res = await axios.post(`http://localhost:5000/api/posts/comment/${postId}/${commentId}`);
+    await axios.delete(`http://localhost:5000/api/posts/comment/${postId}/${commentId}`);
 
     dispatch({
       type: REMOVE_COMMENT,
