32d487f24bb28f1167a03a5c56e25526e55a4503
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-07-07 10:19:31 +0700

    51. Like & Unline Functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,7 +1,5 @@
-import Axios from "axios"
-
 import axios from 'axios';
-import { GET_POSTS, POST_ERROR } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 // Get Posts
 export const getPosts = () => async dispatch => {
@@ -19,3 +17,39 @@ export const getPosts = () => async dispatch => {
     })
   }
 }
+
+// Add like
+export const addLike = id => async dispatch => {
+  try {
+    const res = await axios.put(`http://localhost:5000/api/posts/like/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data }
+    });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    })
+  }
+}
+
+// Remove like
+export const removeLike = id => async dispatch => {
+  try {
+    const res = await axios.put(`http://localhost:5000/api/posts/unlike/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data }
+    });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    })
+  }
+}
+
+
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -2,41 +2,42 @@ import React from 'react'
 import Moment from 'react-moment'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { addLike, removeLike } from '../../actions/post'
 
-const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date } }) => {
+const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date }, addLike, removeLike }) => {
   return (
-    <div class="post bg-white p-1 my-1">
+    <div className="post bg-white p-1 my-1">
       <div>
         <a href="profile.html">
           <img
-            class="round-img"
+            className="round-img"
             src={avatar}
             alt=""
           />
-          <h4>John Doe</h4>
+          <h4>{name}</h4>
         </a>
       </div>
       <div>
-        <p class="my-1">
+        <p className="my-1">
           {text}
         </p>
-        <p class="post-date">
+        <p className="post-date">
           Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
         </p>
-        <button type="button" class="btn btn-light">
-          <i class="fas fa-thumbs-up"></i>{' '}
+        <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
+          <i className="fas fa-thumbs-up"></i>{' '}
           {likes.length > 0 && (
             <span>
               {likes.length}
             </span>
           )}
         </button>
-        <button type="button" class="btn btn-light">
-          <i class="fas fa-thumbs-down"></i>
+        <button onClick={e => removeLike(_id)} type="button" className="btn btn-light">
+          <i className="fas fa-thumbs-down"></i>
         </button>
-        <Link to={`/post/${_id}`} class="btn btn-primary">
+        <Link to={`/post/${_id}`} className="btn btn-primary">
           Discussion {comments.length > 0 && (
-            <span class='comment-count'>
+            <span className='comment-count'>
               {comments.length}
             </span>
           )}
@@ -45,9 +46,9 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
           !auth.loading && user === auth.user._id && (
             <button
               type="button"
-              class="btn btn-danger"
+              className="btn btn-danger"
             >
-              <i class="fas fa-times"></i>
+              <i className="fas fa-times"></i>
             </button>
           )
         }
@@ -60,4 +61,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 })
 
-export default connect(mapStateToProps, null)(PostItem)
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem)
