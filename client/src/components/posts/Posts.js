4bf4e4de648f86b2a7130cf5f6cb20e3cd98c366
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-07-07 09:30:40 +0700

    50. Post Item Component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { getPosts } from '../../actions/post'
 import Spinner from '../layout/Spinner'
+import PostItem from './PostItem'
 
 const Posts = ({ post: { posts, loading }, getPosts }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts])
-  return (
-    <div>
 
-    </div>
+  return loading ? <Spinner /> : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <div className="lead">
+        <i className="fas fa-user"></i> Welcome to the comunity
+      </div>
+      {/* Post Form */}
+      <div className="posts">
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
   )
 }
 
