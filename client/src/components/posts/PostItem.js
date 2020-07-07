7bf1eb4127f9477697dd89ef888eaa80b2a34658
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-07-07 10:49:44 +0700

    52. Deleting Posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Moment from 'react-moment'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { addLike, removeLike } from '../../actions/post'
+import { addLike, removeLike, deletePost } from '../../actions/post'
 
-const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date }, addLike, removeLike }) => {
+const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date }, addLike, removeLike, deletePost }) => {
   return (
     <div className="post bg-white p-1 my-1">
       <div>
@@ -45,6 +45,7 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
         {
           !auth.loading && user === auth.user._id && (
             <button
+              onClick={e => deletePost(_id)}
               type="button"
               className="btn btn-danger"
             >
@@ -61,4 +62,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 })
 
-export default connect(mapStateToProps, { addLike, removeLike })(PostItem)
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(PostItem)
